a453a6456c98ce9dad99887ef807ea97ec3d1e69
Miles <krubbles@gmail.com>
2026-09-19 19:33:49 -0700

    basic impl

--- code/dgemm-basic.cpp
+++ code/dgemm-basic.cpp
@@ -6,7 +6,19 @@ const char* dgemm_desc = "Basic implementation, three-loop dgemm.";
  * where A, B, and C are n-by-n matrices stored in row-major format.
  * On exit, A and B maintain their input values.
  */
-void square_dgemm(int n, double* A, double* B, double* C) 
+void square_dgemm(int n, double* A, double* B, double* C)
 {
-   // insert your code here: implementation of basic matrix multiple
+    for (int cRowIndex = 0; cRowIndex < n; ++cRowIndex)
+    {
+        for (int cColIndex = 0; cColIndex < n; ++cColIndex)
+        {
+            int cIndex = cRowIndex * n + cColIndex;
+            for (int vectorIndex = 0; vectorIndex < n; ++vectorIndex)
+            {
+                int aIndex = cRowIndex * n + vectorIndex;
+                int bIndex = vectorIndex * n + cColIndex;
+                C[cIndex] += A[aIndex] * B[bIndex];
+            }
+        }
+    }
 }
